b53e9def7565f0c13eefa50a02d653638ee1fa4d
Nicklas Peistrup <januspeis@gmail.com>
2022-11-16 10:45:19 +0100

    qs

--- firestarter/LegacyFlows/apfe_release_reuse.cs
+++ firestarter/LegacyFlows/apfe_release_reuse.cs
@@ -39,28 +39,28 @@ jobs:
   deploy:
     name: Build and deploy frontend
     runs-on: ubuntu-latest
-    environment: ${{{{ inputs.environment }}}}
+    environment: ${{ inputs.environment }}
     steps:
       - name: Checkout branch
         uses: actions/checkout@v3
         with:
-          ref: ${{{{ inputs.branch_name }}}}
+          ref: ${{ inputs.branch_name }}
 
       - name: Copy configuration
         id: config
         run: |
-          cp config/${{{{ inputs.environment }}}}/.env .env
+          cp config/${{ inputs.environment }}/.env .env
 
       # Very explicit configure new relic steps. Cannot be included from reuseable file
-      - run: sed -i 's/NEW_RELIC_ACCOUNT_ID/${{{{ secrets.NEW_RELIC_ACCOUNT_ID }}}}/g' public/js/newrelic.js
+      - run: sed -i 's/NEW_RELIC_ACCOUNT_ID/${{ secrets.NEW_RELIC_ACCOUNT_ID }}/g' public/js/newrelic.js
         shell: bash
-      - run: sed -i 's/NEW_RELIC_TRUST_KEY/${{{{ secrets.NEW_RELIC_TRUST_KEY }}}}/g' public/js/newrelic.js
+      - run: sed -i 's/NEW_RELIC_TRUST_KEY/${{ secrets.NEW_RELIC_TRUST_KEY }}/g' public/js/newrelic.js
         shell: bash
-      - run: sed -i 's/NEW_RELIC_AGENT_ID/${{{{ secrets.NEW_RELIC_AGENT_ID }}}}/g' public/js/newrelic.js
+      - run: sed -i 's/NEW_RELIC_AGENT_ID/${{ secrets.NEW_RELIC_AGENT_ID }}/g' public/js/newrelic.js
         shell: bash
-      - run: sed -i 's/NEW_RELIC_LICENSE_KEY/${{{{ secrets.NEW_RELIC_LICENSE_KEY }}}}/g' public/js/newrelic.js
+      - run: sed -i 's/NEW_RELIC_LICENSE_KEY/${{ secrets.NEW_RELIC_LICENSE_KEY }}/g' public/js/newrelic.js
         shell: bash
-      - run: sed -i 's/NEW_RELIC_APPLICATION_ID/${{{{ secrets.NEW_RELIC_APPLICATION_ID }}}}/g' public/js/newrelic.js
+      - run: sed -i 's/NEW_RELIC_APPLICATION_ID/${{ secrets.NEW_RELIC_APPLICATION_ID }}/g' public/js/newrelic.js
         shell: bash
 
       - name: Use npm 
@@ -72,22 +72,22 @@ jobs:
         run: npm install
           
       - name: Run npm build
-        run: npm run build --mode ${{{{ inputs.vue_buildmode }}}} 
+        run: npm run build --mode ${{ inputs.vue_buildmode }} 
 
       - name: Configure AWS credentials
         uses: aws-actions/configure-aws-credentials@v1
         with:
-          aws-access-key-id: ${{{{ secrets.AWS_ACCESS_KEY_ID }}}}
-          aws-secret-access-key: ${{{{ secrets.AWS_SECRET_ACCESS_KEY }}}}
-          aws-region: ${{{{ inputs.region }}}}
+          aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+          aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+          aws-region: ${{ inputs.region }}
 
       - name: Upload to s3
         run: |
-          aws s3 sync dist s3://${{{{ inputs.bucket_name}}}}
+          aws s3 sync dist s3://${{ inputs.bucket_name}}
 
       - name: Invalidate cloudfront cache 
         run: | 
-          aws cloudfront create-invalidation --distribution-id ${{{{ secrets.AWS_DISTRIBUTION_ID }}}} --paths ""/*""
+          aws cloudfront create-invalidation --distribution-id ${{ secrets.AWS_DISTRIBUTION_ID }} --paths ""/*""
 
 ";
 }
