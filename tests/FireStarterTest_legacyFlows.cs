b919445bf50695dfb86ee86adffb6ddafaa92e7a
Nicklas Peistrup <januspeis@gmail.com>
2022-12-28 15:58:25 +0100

    qs

--- tests/FireStarterTest_legacyFlows.cs
+++ tests/FireStarterTest_legacyFlows.cs
@@ -14,8 +14,8 @@ public class FireStarterTest_legacyFlows
         var conf = new ActionInputs
         {
             // Directory = "C:\\Code\\NotificationService",
-            Directory = "C:\\Code\\AuctionService",
-            // Directory = "C:\\Code\\ProductService",
+            // Directory = "C:\\Code\\AuctionService",
+            Directory = "C:\\Code\\ProductService",
             // Directory = "C:\\Code\\ap-frontend",
             //.2
         };
