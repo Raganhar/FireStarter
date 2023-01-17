7bad169eb6d51c0f999f8f0249264d4b9f977645
Nicklas Peistrup <nup@autoproff.dk>
2023-01-17 17:54:33 +0100

    qs

--- run-generator-for-all-repos.ps1
+++ run-generator-for-all-repos.ps1
@@ -1,13 +1,18 @@
-$gitRepos = @("AuctionService","ProductService","tbd-integration-api")
+$PAT=$args[0]
+$gitRepos = @("AuctionService")
 foreach ($repo in $gitRepos) {
-    git clone  ("https://github.com/AUTOProff/" +$repo+".git") 
+
+    git clone ("https://"+$PAT+"@github.com/AUTOProff/" +$repo+".git") --depth 1
     cd $repo
     # git checkout -b update-pipeline
+try{
     dotnet run --project C:\Code\FireStarter\firestarter\
     git add -A
     git commit -a -m 'ran firestarter'
     # git push -u origin update-pipeline
     git push origin 
+} catch{}
+
     cd..
     # rm -R $repo
 }
